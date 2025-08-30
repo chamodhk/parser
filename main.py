10d5c3d2c002fa1dbaf2a0d39df089d3710135b8
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2025-08-30 07:47:53 +0530

    added a working ultra minimal DOM tree builder

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
+from collections import deque
 
-text = """<html>
+
+
+text = """
+<html>
   <head>
-    <title>Test Page</title>
+    <title>Simple Test</title>
   </head>
   <body>
-    <h1 bold>Heading 1</h1>
-    <p italic>This is a paragraph.</p>
+    <h1 bold>Welcome</h1>
+    <p italic>This is the first paragraph.</p>
+    <p>This is the second paragraph with <span bold>some bold text</span> inside.</p>
+    <div>
+      <h2>Subsection</h2>
+      <p>Content inside the subsection.</p>
+    </div>
   </body>
 </html>
+
+
 
 """
 
 
-open_stack = []  #stack to keep opened tags
+open_tag_stack = [None]  #stack to keep opened tags
+text_stack = [None]
+
+
+
+Root = None
 
 class Node:
     def __init__(self, tag_name, tag_attribs):
         self.tag_name = tag_name 
         self.tag_attribs = tag_attribs
+        self.tag_content= ""
         self.children = []
+
+
+    # def __str__(self):
+    #     return self.tag_name
 
     
 
@@ -34,7 +55,6 @@ while i < len(text):
                 tag += text[i]
                 i += 1
             tag += ">"
-            print(tag)
             
             j = 1
             tag_name = ""
@@ -50,15 +70,60 @@ while i < len(text):
                 j += 1
 
             # print(tag_name)
-            print(tag_attribs)
+            # print(tag_attribs)
 
             e = Node(tag_name,tag_attribs)
-            open_stack.append(e)
+            open_tag_stack.append(e)
+
+            text_stack.append("")
         else:
             #closing tag 
-            pass
+            closing_tag = ""
+            i+= 2
+            while (text[i] != ">"):
+                closing_tag += text[i]
+                i += 1
+                
+            # print(closing_tag)
+            
+            opening_tag = open_tag_stack.pop()
+            tag_contet = text_stack.pop()
+            if opening_tag.tag_name == closing_tag:
+                opening_tag.content = tag_contet
+            else:
+                print("tag mismatch")
+
+            if (open_tag_stack[-1]):
+                open_tag_stack[-1].children.append(opening_tag)
+            else:
+                Root = opening_tag
+
+
     else:
         #text token
-        pass
+        if (text_stack[-1]):
+            text_stack[-1] += (text[i])
 
     i += 1
+
+
+
+def dfs(node, depth):
+    if (depth == 0):
+        prefix = ""
+    else:
+        prefix = "|" + "-"*depth
+    print(prefix + node.tag_name)
+    if (node):
+        depth += 1
+        child_count = len(node.children)
+        for i in range(child_count):
+            dfs(node.children[i], depth)
+
+
+
+node = Root
+
+
+
+dfs(node,0)
